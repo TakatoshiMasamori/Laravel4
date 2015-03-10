e2277bb8d894da2aa715f90f723fa83be4a6befd
Takatoshi Masamori <s.-.s@i.softbank.jp>
2015-03-10 17:08:17 +0900

    Laravel4

---
+++ app/config/services.php
@@ -0,0 +1,31 @@
+<?php
+
+return array(
+
+	/*
+	|--------------------------------------------------------------------------
+	| Third Party Services
+	|--------------------------------------------------------------------------
+	|
+	| This file is for storing the credentials for third party services such
+	| as Stripe, Mailgun, Mandrill, and others. This file provides a sane
+	| default location for this type of information, allowing packages
+	| to have a conventional place to find your various credentials.
+	|
+	*/
+
+	'mailgun' => array(
+		'domain' => 'nous.ne.jp',
+		'secret' => 'key-b822d6a30cce248cb86cb44f79e0ac66',
+	),
+
+	'mandrill' => array(
+		'secret' => '',
+	),
+
+	'stripe' => array(
+		'model'  => 'User',
+		'secret' => '',
+	),
+
+);
